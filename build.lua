91a5ca8d48a718287a38feda4b365a88a833ff5f
Frank Mittelbach <frank.mittelbach@latex-project.org>
2018-12-31 16:29:40 +0100

    AMS classes are maintained by the AMS

--- build.lua
+++ build.lua
@@ -9,6 +9,31 @@ module = "latexbug"
 -- Release a TDS-style zip
 packtdszip = true
 
+
+
+-- Upload meta data
+
+uploadconfig = {
+ pkg = "latexbug",
+ version = "v1.0d 2018-12-31",
+ author = "The LaTeX Team",
+ license = "lppl1.3c",
+ summary = "Bug-classification for LaTeX related bugs",
+ ctanPath = "/macros/latex/required/latexbug",
+ repository = "https://github.com/latex3/latexbug",
+ uploader = "Frank Mittelbach",
+ email = "latex-team@latex-project.org",
+ update = true ,
+ note = [[Uploaded automatically by l3build...]],
+}
+
+
+checkruns = 2  -- needed for hyperref tests to gen all help files
+
 -- Find and run the build system
-kpse.set_program_name ("kpsewhich")
-dofile (kpse.lookup ("l3build.lua"))
+
+if not release_date then
+   kpse.set_program_name("kpsewhich")
+   dofile(kpse.lookup("l3build.lua"))
+end
+
