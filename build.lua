bba6a0ef410d3b0bcd2accd767048135174c6238
Frank Mittelbach <frank.mittelbach@latex-project.org>
2021-09-25 11:12:22 +0200

    take expl3.ltx out of the warnings (shows up in rollback)

--- build.lua
+++ build.lua
@@ -15,7 +15,7 @@ packtdszip = true
 
 uploadconfig = {
  pkg = "latexbug",
- version = "v1.0k 2021-06-28",
+ version = "v1.0l 2021-09-25",
  author = "The LaTeX Team",
  license = "lppl1.3c",
  summary = "Bug-classification for LaTeX related bugs",
